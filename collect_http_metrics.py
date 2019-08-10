85df11c103e27f6e4236d2a0562081c4b9c4b9f0
deafnill <deafnill@gmail.com>
2019-08-09 20:35:09 -0500

    another commit

--- collect_http_metrics.py
+++ collect_http_metrics.py
@@ -6,32 +6,43 @@ import datetime
 import yaml
 import threading
 
-
+"""Class Used for Metric Collection Of Web Applications"""
 class HTTPMetricCollector(object):
     def __init__(self,interval, targethost,targetport,ssl_bool,log_file_name,request_file_name,site_name,site_number,site_region,application_name,max_log_size,max_connection_thread_count,debug=False,print_out=True):
+        """debug only used to log http request and response metrics to file"""
         self.debug = debug
+        """print used to output http request and response metrics to terminal"""
         self.print_out = print_out
-        self.max_connection_thread_count = max_connection_thread_count
-        self.application_name = application_name
+
+        """Site config information"""
         self.site_region = site_region
         self.site_number = site_number
         self.site_name = site_name
+        self.host_name = socket.gethostname()
+        self.host_ip = socket.gethostbyname(self.host_name)
+
+        """application collection config information"""
+        self.application_name = application_name
         self.targethost = targethost
         self.targetport = targetport
         self.ssl_bool = ssl_bool
         self.interval = interval
-        self.context = ssl.create_default_context()
-        self.host_name = socket.gethostname()
-        request_file = open("Requests\\" + request_file_name,"r")
-        user_agent = "%s.%s.%s" % (str(self.site_region),str(self.site_number),str(self.site_name))
         self.max_log_size = max_log_size
-        self.log_file_name = log_file_name;
+        self.log_file_name = log_file_name
+        """flag used to stop threads if exceeding max"""
+        self.stop_threads = False
+        """max con to keep threads from creating a mem leak"""
+        self.max_connection_thread_count = max_connection_thread_count
+        """build application request"""
+        request_file = open("Requests\\" + request_file_name, "r")
+        user_agent = "%s.%s.%s" % (str(self.site_region), str(self.site_number), str(self.site_name))
         self.encoded_request = request_file.read().replace("<user-agent-generated>",
                                                            user_agent).encode()
-        self.host_ip = socket.gethostbyname(self.host_name)
-        self.stop_threads = False
 
+        """prepare ssl context for use"""
+        self.context = ssl.create_default_context()
 
+    """log function to simplify some of the code"""
     def log(self,message=""):
         self.log_file = open("logs\\" + self.log_file_name, "a+")
         if self.log_file.__sizeof__() > self.max_log_size:
@@ -39,6 +50,7 @@ class HTTPMetricCollector(object):
         self.log_file.write(message)
         self.log_file.close()
 
+    """used to start threading application connections"""
     def start_collection(self):
         self.log()
         threads = []
@@ -54,13 +66,17 @@ class HTTPMetricCollector(object):
                 self.log(ex)
                 time.sleep(self.interval)
 
+    """function used in threading to collect metrics"""
     def collect_http_application_metric(self):
-        time_stamp = datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S")
+        """timestamp start of every thread"""
+        start_time = datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S")
+        """timestamp before tcp handshake"""
         start_tcp_handshake = time.time()
         with socket.create_connection((self.targethost, self.targetport)) as sock:
+            """delta timestamp after tcp handshake"""
             cur_tcp_handshake_rtt = format((time.time() - start_tcp_handshake) * 1000,'.2f')
+            """get connected ip address and connected port"""
             target_ip, target_port = sock.getpeername()
-
             if self.ssl_bool:
                 """If ssl is configured for this application"""
                 """timestamp before ssl negotiation"""
@@ -76,7 +92,6 @@ class HTTPMetricCollector(object):
                     cur_request_to_return_rtt = format(float(time.time() - start_application_request) * 1000, '.2f')
                     ssl_ver = ssock.version()
                     ssock.close()
-
             else:
                 """If ssl is configured for this application"""
                 cur_ssl_negotiation_rtt = 0
@@ -98,7 +113,7 @@ class HTTPMetricCollector(object):
         cur_total_transaction_rtt = format(float(cur_total_net_time_rtt) + float(cur_request_to_return_rtt),'.2f')
         """organize metrics in json output, prepared to store or transfer"""
         response_metrics = {
-            "timestamp": str(time_stamp),
+            "start_time": str(start_time),
             "application_name": self.application_name,
             "interval_timer": str(self.interval),
             "localhost":{
@@ -124,7 +139,6 @@ class HTTPMetricCollector(object):
                 "site_number":self.site_number,
                 "site_region":self.site_region,
                    }
-
         }
 
         if self.print_out:
@@ -135,8 +149,6 @@ class HTTPMetricCollector(object):
         if self.debug:
             """save metrics collected if debugging"""
             self.log("DEBUG:%s\n" % response_metrics)
-        else:
-            self.log()
 
 if __name__ == '__main__':
     with open("config.yml", 'r') as yamlfile:
